74c505fffe0ea703cb9f7fa324e3bb07443106b8
Snypie <doworkmarine@gmail.com>
2018-08-01 16:06:54 +0900

    installed booststrap and fixed jquery bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,11 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require popper
+//= require jquery3
+//= require jquery_ujs
+//= require bootstrap-sprockets
+//= require bootstrap
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
