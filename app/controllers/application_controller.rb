fe9066058f421fcc5eebdf4c7a0e9af9b4d88fad
Preston Moore <doworkmarine@gmail.com>
2018-08-02 14:26:51 +0900

    installed petergate gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+  	@copyright = SnypieViewTool::Renderer.copyright 'Preston Moore', 'All rights Reserved'
+	end
+end
+
+module SnypieViewTool
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end
+	end
 end
